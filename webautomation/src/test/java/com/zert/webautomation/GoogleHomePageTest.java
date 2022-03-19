07895e645b186636debee2c2a26d172cc88bf137
Arunprasath Rajmohan <arunprasath.kamakshi@gmail.com>
2022-03-19 20:29:39 +1000

    SAM-1 Added comments

--- webautomation/src/test/java/com/zert/webautomation/GoogleHomePageTest.java
+++ webautomation/src/test/java/com/zert/webautomation/GoogleHomePageTest.java
@@ -11,7 +11,7 @@ public class GoogleHomePageTest {
 @Test
 
 	public void homePageTest() {
-	
+	// Set Driver Manager before creating driver instance
     WebDriverManager.chromedriver().setup();
 	WebDriver driver=new ChromeDriver();
 	driver.get("https://www.google.com/");
